5957ebeb4d4173a29f9e60aae537c53354d1f3e0
Dadumoni <165320151+Dadumoni@users.noreply.github.com>
2025-04-23 18:33:28 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -15,22 +15,22 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '29382018'))
-    API_HASH = str(getenv('API_HASH', '4734a726c04620c61ec0a28a1ae0d57f'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7592800776:AAHQG1VJ-9l-VncP4ojwh0pfCioyr_swp6w'))
+    API_ID = int(getenv('API_ID', '20845715'))
+    API_HASH = str(getenv('API_HASH', '72b3a12322c4edeb6c35fe91376aae84'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '8134149902:AAHazHJuq4SA2LLu2gTLEl8X3qcILEDFcII'))
     PICS = (environ.get('PICS', 'https://envs.sh/jUp.jpg')).split()
     name = str(getenv('name', 'F2l_RoboT'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002428562251'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002428562251'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002308870921'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002638464472'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "7442532306").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "7340459339").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'L_abani'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'bitul_kr'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
@@ -43,8 +43,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'noob_project')) 
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://dadumoni05:rvDiSmEwBagFqWDZ@cluster0.3wcnpfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Thrill_Zone')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ @L_abani ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
